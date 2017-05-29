4edfa913c32acf0aa12645d9744065ae93ab520d
LeeWooSang <lus0505@naver.com>
2017-05-29 09:33:06 +0900

    런쳐기능추가,지역 검색기능 구현

---
+++ launcher.py
@@ -0,0 +1,32 @@
+from project1 import *
+
+loopFlag = 1
+def printMenu():
+    print("Hi! This is performance,exhibit search program ")
+    print("######################Menu#####################")
+    print("Data search: d")
+    print("Area search: a")
+    print("Quit: q")
+    print("###############################################")
+
+
+def launcherFunction(menu):
+    if menu == 'd':
+        Date_Showing_Parsing()
+    elif menu == 'a':
+        Area_Showing_Parsing()
+    elif menu == 'q':
+        Quit()
+        print("Good bye!")
+
+
+def Quit():
+    global loopFlag
+    loopFlag = 0
+
+while(loopFlag > 0):
+    printMenu()
+    menuKey = str(input("select Menu:"))
+    launcherFunction(menuKey)
+
+
